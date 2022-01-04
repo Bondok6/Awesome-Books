b7dff8b109362afc5dd80a16158965265f31b166
Bondok6 <kirolousbondok6@gmail.com>
2022-01-04 14:41:38 +0200

    Preseve Data in the browser's memory by using 'localStorage'

--- main.js
+++ main.js
@@ -14,35 +14,46 @@ const addBtn = document.querySelector(".add-btn");
 const titleInput = document.querySelector(".title-input");
 const authorInput = document.querySelector(".author-input");
 
+// Data Storage
+const storage = function (books) {
+  localStorage.setItem("books", JSON.stringify(books));
+};
+
 // Add Book
 const addBook = function (title, author) {
   const newBook = new Books(title, author);
   books.push(newBook);
+  storage(books);
 };
 
 // Remove Book
 const removeBook = function (title) {
   books = books.filter((book) => book.title !== title);
+  storage(books);
 };
 
 // Display Book
 const displayBook = function () {
-  let itemHtml = `
-  <li class="book">
-    <div class="title">${books[books.length - 1].title}</div>
-    <div class="author">${books[books.length - 1].author}</div>
-    <button type="button" class="remove-btn">Remove</button>
-  </li>
-`;
+  books = JSON.parse(localStorage.getItem("books"));
 
-  container.insertAdjacentHTML("beforeend", itemHtml);
+  let itemHtml = ``;
+  books.forEach((book) => {
+    itemHtml += `
+    <li class="book">
+      <div class="title">${book.title}</div>
+      <div class="author">${book.author}</div>
+      <button type="button" class="remove-btn">Remove</button>
+    </li>
+  `;
+  });
+
+  container.innerHTML = itemHtml;
 
   //Remove when I click on Remove button
   document.querySelectorAll(".remove-btn").forEach((btn) => {
     btn.addEventListener("click", (e) => {
       let targetTitle =
         e.target.parentElement.firstChild.nextSibling.textContent;
-      console.log(targetTitle);
       removeBook(targetTitle);
       e.target.parentElement.remove();
     });
@@ -51,8 +62,13 @@ const displayBook = function () {
 
 // Add when I click on Add button
 addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   let title = titleInput.value;
   let author = authorInput.value;
   addBook(title, author);
   displayBook();
 });
+
+window.onload = () => {
+  displayBook();
+};
